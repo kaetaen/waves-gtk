27da072e89a5de11234fbb21d8596c20fcf1d08c
kaetaen <kaetaen@tutanota.com>
2023-01-15 22:12:11 -0300

    feat: add verify if pulseaudio is installed

--- src/main.py
+++ src/main.py
@@ -3,17 +3,19 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 import os
 
+
 class MyWindow(Gtk.Window):
     def __init__(self):
+        self.verify_pulseaudio_installation()
         self.activeLoopback = False
 
-        Gtk.Window.__init__(self, title="Meu botão")
+        Gtk.Window.__init__(self, title="Waves")
         self.set_default_size(200, 300)
 
-        btn = Gtk.Button(label="WAves")
+        btn = Gtk.Button(label="Listen")
         btn.set_halign(Gtk.Align.CENTER)
         btn.set_valign(Gtk.Align.CENTER)
-        btn = Gtk.Button("Clique aqui")
+        btn = Gtk.Button("Click Here")
         btn.connect("clicked", self.on_btn_clicked)
 
         self.add(btn)
@@ -28,6 +30,15 @@ class MyWindow(Gtk.Window):
             os.system("pactl load-module module-loopback latency_msec=1")
         
 
+    def verify_pulseaudio_installation(self):
+        if not (os.path.exists('/usr/bin/pactl') or os.path.exists('/bin/pactl')):
+            message_dialog = Gtk.MessageDialog(parent=None,
+                                  flags=0,
+                                  message_type=Gtk.MessageType.WARNING,
+                                  buttons=Gtk.ButtonsType.OK,
+                                  text="PulseAudio is not installed!")
+            message_dialog.run()
+            exit()
     
 win = MyWindow()
 win.connect("destroy", Gtk.main_quit)
